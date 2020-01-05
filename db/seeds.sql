9fd0c235a72fa3e9b9acbd3080cf88885f4f4e18
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2020-01-04 21:15:48 -0500

    Added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+  
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pizza Burger", FALSE),
+  ("Bacon Burger", FALSE),
+  ("Beefy Burger", FALSE),
+  ("Teenage Mutant Ninija Turtle Vitamin Gummy Burger", FALSE),
+  ("Broken Burger", FALSE)
